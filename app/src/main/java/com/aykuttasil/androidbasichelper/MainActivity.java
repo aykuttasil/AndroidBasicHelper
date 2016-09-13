565761c3703d3a031f8ff9f473b34ec08ef359c5
Aykut Asil <aykuttasil@gmail.com>
2016-09-13 20:42:35 +0300

    Utils. V1.0.21

--- app/src/main/java/com/aykuttasil/androidbasichelper/MainActivity.java
+++ app/src/main/java/com/aykuttasil/androidbasichelper/MainActivity.java
@@ -1,7 +1,10 @@
 package com.aykuttasil.androidbasichelper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.blankj.utilcode.utils.AppUtils;
+import com.blankj.utilcode.utils.DeviceUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,8 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
     }
 }
